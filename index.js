b2ef78c8556b1c71d30ef27940958b672ef5fd2d
Aline Duarte Fernandes <115157598+alinedfernandes27@users.noreply.github.com>
2023-12-02 16:16:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let nomeHeroi= "Loki"
+let XP = 7159
+let nivelHeroi= "Platina"
+
+if (XP >= 7001 & <= 8000){
+nivelHeroi="Platina";
+
+}else if(XPHeroi <= 1000){
+    nivelHeroi="Ferro";
+
+}else if(XPHeroi <= 1001 & >= 2000){
+    nivelHeroi="Bronze";
+
+}else if(XPHeroi >= 2001 & <= 6000){
+    nivelHeroi="Prata";
+
+}if(XPHeroi >= 6001 & <= 7000){
+    nivelHeroi="Ouro";
+
+}if(XPHeroi >= 8001 & <= 9000){
+nivelHeroi="Ascendente";
+
+}if(XPHeroi >= 9001 & <= 10.000){
+    nivelHeroi="Imortal";
+
+}if(XPHeroi>= 10.001){
+    nivelHeroi="Radiante";
+
+}else{
+    resultado=7159
+
+}
+
+console.log("O Herói de nome Loki está no nível de Platina")
